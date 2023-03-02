d5574159f73201d9680c8cf6b93006b128c8078d
Abdul <aabdulmukhsin@yahoo.com>
2023-03-02 09:18:21 +0000

    added animation

--- src/components/Homepage/Challenges.jsx
+++ src/components/Homepage/Challenges.jsx
@@ -12,7 +12,7 @@ const Challenges = () => {
                 </div>
                 <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
                     {
-                        challenges.map(challenge => <div className="shadow-lg w-[60%] mr-auto ml-auto py-10 px-5 rounded-lg" style={{ backgroundImage: `${challenge.cardBg}` }}>
+                        challenges.map(challenge => <div className="shadow-lg lg:w-[60%] w-full mr-auto ml-auto py-10 px-5 rounded-lg" style={{ backgroundImage: `${challenge.cardBg}` }}>
                             <p className="text-center font-semibold">{challenge.title}</p>
                             <p className="text-center">{challenge.description}</p>
                         </div>)
--- src/components/Homepage/Navbar.jsx
+++ src/components/Homepage/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
                 <img src={Logo} alt="logo" width={"100px"} />
             </div>
             <div className="flex flex-wrap items-center">
-                <ul className="flex flex-wrap">
+                <ul className="flex flex-wrap lg:justify-start justify-center">
                     {
                         navs.map((nav) => <li className="mr-3 text-sm">
                             <Link to={"/"}>{nav.name}</Link>
@@ -18,8 +18,8 @@ const Navbar = () => {
                     }
 
                 </ul>
-                <div>
-                    <button className="bg-primary_blue text-white px-4 py-3 rounded-lg">Join Waitlist</button>
+                <div className="lg:block hidden">
+                    <button className="bg-primary_blue  text-white px-4 py-3 rounded-lg">Join Waitlist</button>
                 </div>
             </div>
         </div>
